771ef0fc4c3bc65cb8f57d5399c30c2c6c8ce365
skazu37 <e1920037@oit.ac.jp>
2022-08-23 00:15:57 +0900

    チャット画面の修正

--- src/components/Answer.jsx
+++ src/components/Answer.jsx
@@ -4,7 +4,15 @@ import React from "react";
 const Answer =(props)=>{
     return(
         <Stack spacing={2} direction="row">
-            <Button variant="contained" color="primary" onClick={()=>props.select(props.content,props.nextId)}>
+            <Button sx={{borderColor:"#FFB549",
+            color:"#FFB549",
+            fontWeight:600,
+            marginBottom:"8px",
+            width:"100%"
+        
+        }} 
+            variant="outlined" 
+            onClick={()=>props.select(props.content,props.nextId)}>
                 {props.content}
             </Button>
         </Stack>
--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -4,7 +4,7 @@ import Chat from "./Chat";
 
 const Chats=(props)=>{
     return(
-        <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+        <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper',height: 400,padding: "0", overflow:"auto"}}>
             {props.chats.map((chat,index)=>{
                 return <Chat text={chat.text} type={chat.type} key={index}/>
             })}
